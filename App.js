f293bdf3731435d31ccd4db7d3b94df0b16c06f6
darkoisaac20 <darkoisaac20@gmail.com>
2020-02-24 21:57:49 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{padding: 50, flexDirection: 'row', height: 300, weight: '80%', justifyContent: 'space-between', alignItems: 'stretch'}} >
+      <View style={{
+        backgroundColor: 'red',
+        flex: 2,
+        justifyContent: 'center',
+        alignItems:'center'
+
+      }}
+      >
+        <Text>1</Text>
+
+      </View>
+
+      <View style={{
+        backgroundColor: 'blue',
+        flex: 3,
+        justifyContent: 'center',
+        alignItems:'center'
+
+      }}
+      >
+        <Text>2</Text>
+
+      </View>
+
+      <View style={{
+        backgroundColor: 'green',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems:'center'
+
+      }}
+      >
+        <Text>3</Text>
+
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+   
+  
+});
