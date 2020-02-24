d7f6269eaae54818019c6391484efd509313ca6e
darkoisaac20 <darkoisaac20@gmail.com>
2020-02-24 22:37:55 +0000

    first playground view...lol

--- App.js
+++ App.js
@@ -3,40 +3,13 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 50, flexDirection: 'row', height: 300, weight: '80%', justifyContent: 'space-between', alignItems: 'stretch'}} >
-      <View style={{
-        backgroundColor: 'red',
-        flex: 2,
-        justifyContent: 'center',
-        alignItems:'center'
+    <View style={styles.screen} >
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input}/>
+        <Button title="ADD" />
+      </View> 
 
-      }}
-      >
-        <Text>1</Text>
-
-      </View>
-
-      <View style={{
-        backgroundColor: 'blue',
-        flex: 3,
-        justifyContent: 'center',
-        alignItems:'center'
-
-      }}
-      >
-        <Text>2</Text>
-
-      </View>
-
-      <View style={{
-        backgroundColor: 'green',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems:'center'
-
-      }}
-      >
-        <Text>3</Text>
+      <View>
 
       </View>
     </View>
@@ -44,6 +17,20 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-   
-  
+   screen:{
+     padding:50
+   },
+
+   input: {
+    width:'80%',
+     borderColor: 'black', 
+     borderWidth: 1,
+     padding:7
+   },
+
+   inputContainer: {
+     flexDirection: 'row',
+      justifyContent: 'space-between', 
+      alignItems: 'center'
+    }
 });
