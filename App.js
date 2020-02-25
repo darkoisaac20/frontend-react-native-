c79940a3c3c253b27d5905109462361461602474
darkoisaac20 <darkoisaac20@gmail.com>
2020-02-25 01:07:57 +0000

    a simple goals app built on react native

--- App.js
+++ App.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+  
+
+const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () =>{
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  };
+
+
   return (
+    <ScrollView>
     <View style={styles.screen} >
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.input}/>
-        <Button title="ADD" />
-      </View> 
-
-      <View>
-
-      </View>
+        <TextInput placeholder="Course Goal" 
+        style={styles.input}
+        onChangeText={goalInputHandler}
+        value = {enteredGoal}
+        />
+        <Button title="ADD"  onPress={addGoalHandler}/>
+      </View>   
+      <ScrollView >
+      {courseGoals.map((goal) => 
+              <View  key={goal} style={styles.listItem}>
+              <Text>{goal}</Text>
+              </View>
+        )}
+      </ScrollView>
     </View>
+    </ScrollView>
   );
 }
 
@@ -32,5 +54,12 @@ const styles = StyleSheet.create({
      flexDirection: 'row',
       justifyContent: 'space-between', 
       alignItems: 'center'
+    },
+    listItem:{
+      backgroundColor: '#ccc',
+      borderColor: 'black',
+      padding: 10,
+      marginVertical: 10,
+      borderWidth: 1
     }
 });
